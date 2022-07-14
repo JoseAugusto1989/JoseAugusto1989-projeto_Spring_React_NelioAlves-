8d837b825893574750f7eae04aca453498241659
Jose Augusto de Oliveira Pins <joseaugusto@ciandt.com>
2022-07-14 10:46:18 -0300

    criando service, controller e repository

---
+++ backend/src/main/java/com/ciandt/backend/controller/SaleController.java
@@ -0,0 +1,28 @@
+package com.ciandt.backend.controller;
+
+import com.ciandt.backend.entities.Sale;
+import com.ciandt.backend.service.SaleService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+    @Autowired
+    private SaleService saleService;
+
+    @GetMapping
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return saleService.findSales(minDate, maxDate, pageable);
+    }
+}
